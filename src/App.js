5a04aaa8035f2bef7598d0d329b8b3380cb9be2d
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-20 18:43:34 +0800

    Updated parts into created components

--- src/App.js
+++ src/App.js
@@ -1,82 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import GameStatsTable from './GameStatsTable';
+import UserWin from './UserWin';
 import './App.css';
 
 function App() {
 	const [guessCount, setGuessCount] = useState(0);
-	const [bullCount, setBullCount] = useState(0);
-	const [cowCount, setCowCount] = useState(0);
 	const [userInput, setUserInput] = useState('');
-	const [userGuess, setUserGuess] = useState('');
 	const [secretNumber, setSecretNumber] = useState('');
-	const [win, setWin] = useState(false);
+	const [userGuessList, setUserGuessList] = useState([]);
+	const [userWin, setUserWin] = useState(false);
 	const [showError, setShowError] = useState(true);
 
-	// Generate 4-digit number (UNIQUE DIGITS)
 	useEffect(() => {
-		let integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-		let secretNumber = '';
-
-		let rand = Math.floor(Math.random() * 9) + 1;
-		let digits = [rand];
-		integers.splice(integers.indexOf(rand), 1);
-
-		for (let i = 0; i < 3; i++) {
-			rand = Math.floor(Math.random() * integers.length);
-			digits.push(integers[rand]);
-			integers.splice(rand, 1);
-		}
-
-		digits.forEach((digit) => {
-			secretNumber = secretNumber + digit;
-		});
-
-		setSecretNumber(secretNumber);
+		setSecretNumber(generateSecretNumber);
 	}, []);
 
+	// On user submit, validate user input
 	const submitUserGuess = () => {
-		let bull = 0;
-		let cow = 0;
-
-		if (showError) return alert('Input not a 4-digit number!');
-		for (let i = 0; i < 4; i++) {
-			for (let j = 0; j < 4; j++) {
-				if (userInput[i] === userInput[j] && i !== j) return alert('Digits are not unique!');
-			}
-		}
+		// Input error handling
+		if (isNaN(userInput)) return alert('Input is not a valid number!');
+		if (isError(showError)) return alert('Input is not a 4-digit number!');
+		if (isRepeating(userInput)) return alert('Input digits are not unique!');
 
 		setGuessCount((guessCount) => guessCount + 1);
-		setUserGuess(userInput);
 
-		if (userInput.length !== 4) {
-			console.log('Not submitted. Guess must be 4-digits!', userGuess);
-		} else {
-			console.log('This userGuess was submitted:', userGuess);
-		}
+		let currentGuess = checkGuess(userInput, secretNumber);
+		if (currentGuess.bullCount === 4) setUserWin(true);
 
-		
-
-		for (let i = 0; i < userInput.length; i++) {
-			if (secretNumber.includes(userInput[i]))
-				secretNumber[i] === userInput[i] ? bull++ : cow++;
-		}
-		setBullCount(bull);
-		setCowCount(cow);
-
-		if (bull === 4) setWin(true);
+		setUserGuessList((userGuessList) => [currentGuess, ...userGuessList]);
 	};
 
-	const checkUserInput = (e) => {
-		const value = e.target.value;
-		if (value.length !== 4) {
-			console.log('Guess must be 4-digits!');
-			setShowError(true);
-		} else {
-			setShowError(false);
-			setUserInput(value);
-			console.log('userInput is set to', value);
-		}
+	// Show error until user has typed 4 characters
+	const checkInputError = (value) => {
+		if (value.length !== 4) return setShowError(true);
+		setShowError(false);
+		setUserInput(value);
 	};
 
+	// On form submit, reset only the input field
+	const submitFormHandler = (e) => {
+		e.preventDefault();
+		e.target.reset();
+	};
+
+	// Refresh page
 	const playAgain = () => {
 		window.location.reload();
 	};
@@ -87,42 +54,100 @@ function App() {
 				<h1>BULLS AND COWS</h1>
 			</header>
 
-			{!win && (
-				<div className='Input-guess'>
-					<div className='User-input'>
-						<input
-							type='text'
-							autoComplete='off'
-							name='userGuess'
-							onChange={(e) => checkUserInput(e)}
-						/>
-						<button onClick={submitUserGuess}>Guess</button>
+			{!userWin && (
+				<form className='Form' onSubmit={(e) => submitFormHandler(e)}>
+					<div className='Input-guess'>
+						<div className='User-input'>
+							<input
+								className='Input-field'
+								name='userGuess'
+								type='text'
+								maxLength='4'
+								autoComplete='off'
+								onChange={(e) => checkInputError(e.target.value)}
+							/>
+							<input
+								className='Submit-btn'
+								type='submit'
+								value='Guess'
+								onClick={submitUserGuess}
+							/>
+						</div>
+						<div className='Error-message'>
+							{showError && <div className='Error'>Enter 4 unique digits</div>}
+						</div>
 					</div>
-					<div className='Error-message'>
-						{showError && <div className='Error'>Guess must be 4-digits!</div>}
-					</div>
-				</div>
+				</form>
 			)}
 
-			{win && (
-				<div className='User-won'>
-					<h1>You guessed it right!</h1>
-					<div className='Secret-number'>
-						<h2>Secret Number: {secretNumber}</h2>
-					</div>
-					<button onClick={playAgain}>Play Again</button>
-				</div>
+			{userWin && (
+				<UserWin
+					secretNumber={secretNumber}
+					guessCount={guessCount}
+					clickAction={playAgain}
+				/>
 			)}
 
-			<div className='Game-stats'>
-				<h3>Total Guesses: {guessCount}</h3>
-				<h3>User guess: {userGuess}</h3>
-				<h3>
-					Bulls: {bullCount} Cows: {cowCount}
-				</h3>
+			<div className='Game-stats-container'>
+				<div className='Scroll-table'>
+					<GameStatsTable userGuessList={userGuessList} />
+				</div>
 			</div>
 		</div>
 	);
 }
 
+// Generate a 4-digit number (non-repeating digits)
+const generateSecretNumber = () => {
+	let integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	let secretNumber = String(Math.floor(Math.random() * 9) + 1);
+	integers.splice(integers.indexOf(Number(secretNumber)), 1);
+
+	for (let i = 0; i < 3; i++) {
+		let idx = Math.floor(Math.random() * integers.length);
+		secretNumber += integers[idx];
+		integers.splice(idx, 1);
+	}
+
+	// For checking purposes
+	console.log('Secret Number is', secretNumber);
+	return secretNumber;
+};
+
+// Check if not a number
+const isNaN = (testInput) => {
+	if (!testInput.match(/^[1-9]\d*$/g)) return true;
+	return false;
+};
+
+// Check if there is input error (length !== 4)
+const isError = (testError) => {
+	if (testError) return true;
+	return false;
+};
+
+// Check if digits are repeating
+const isRepeating = (testInput) => {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			if (testInput[i] === testInput[j] && i !== j) {
+				return true;
+			}
+		}
+	}
+	return false;
+};
+
+// Count number of bulls and cows
+const checkGuess = (guess, number) => {
+	let bull = 0;
+	let cow = 0;
+	for (let i = 0; i < guess.length; i++) {
+		if (number.includes(guess[i])) number[i] === guess[i] ? bull++ : cow++;
+	}
+
+	return { userGuess: guess, bullCount: bull, cowCount: cow };
+};
+
+export { generateSecretNumber, isNaN, isError, isRepeating, checkGuess };
 export default App;
